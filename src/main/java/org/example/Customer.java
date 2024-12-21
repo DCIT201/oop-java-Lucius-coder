96304109dc49c5872c7559cca50e6478f8a84c25
Lucius Jackson <luciusjackson5@gmail.com>
2024-12-21 15:23:33 +0000

    submitting assignment

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -4,7 +4,7 @@ class Customer {
     private String customerId;
     private String name;
 
-    public Customer(String customerId, String name) {
+    public  Customer(String customerId, String name) {
         this.customerId = customerId;
         this.name = name;
     }
